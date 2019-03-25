2c661ca445aa1e5fe108777b68cae69eb25f6d7c
h f <waltermelonson@gmail.com>
2019-03-24 21:05:20 -0700

    small changes while sick

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,20 +25,11 @@ $(document).ready(function () {
   makeButtons();
 
 
-    $(".add").click(function(event){
-        event.preventDefault();
-        var newSearch = $("input").val();
-        movies.push(newSearch);
-        makeButtons();
-
-        console.log(movies);
-    })
-
-    $(".term").click(".buttons-display", function(){
+   $(".term").click(".buttons-display", function(){
 
         var select = ($(this).val());
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + select + "&limit=10";
-       
+        
         $.ajax({
             url: queryURL,
             method: "GET",
@@ -48,18 +39,61 @@ $(document).ready(function () {
             console.log(res);
            for(var j = 0; j < res.data.length; j++){
                 var content = $("<img>");
+                content.addClass("gif");
                 var rating = $("<p>").text("Rating: " + res.data[j].rating);
                 console.log(res);
               //  content.text(res.data.rating);
                 content.attr("src", res.data[j].images.fixed_height.url);
                 $(".gifs-display").append(rating);
                 $(".gifs-display").append(content);
-           }
-            
+
+                }
+                $(".gif").click(function(){
+
+                    var status = $(this).attr("status");
+
+                    if(status === "moving"){
+                        $(this).attr("src", res.data[j].images.fixed_height.url);
+                        $(this).attr("status", "moving");
+                    }
+                    else{
+                        
+
+                        $(this).attr("src", res.data[j].images.fixed_height_still);
+                        $(this).attr("status", "stop")
+
+                    }
+
+
+                })
+
+
+           
+            makeButtons();
+
+
+            $(".add").click(function(event){
+        event.preventDefault();
+        var newSearch = $("input").val();
+        movies.push(newSearch);
+        makeButtons();
+
+        console.log(movies);
+    })
+
+
+
+
         })
         console.log(select);
-        //makeButtons();
+        
     })
+
+    
+    
+   
+
+   
 
     
 });
